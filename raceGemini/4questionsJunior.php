c8960502c71bdbb043a6bc7901977a2778fa5d17
MrEuclid <john@teacherjohn.org>
2026-03-12 13:57:00 +0700

    Add files via upload

--- raceGemini/4questionsJunior.php
+++ raceGemini/4questionsJunior.php
@@ -7,7 +7,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Progressive 4 Equations Middle</title>
+  <title>Progressive Linear Equations</title>
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -25,7 +25,7 @@
 
   <style>
     .game-container { text-align: center; margin-top: 20px; }
-    #equation-display { font-size: 2.2em; margin: 30px 0; min-height: 80px; }
+    #equation-display { font-size: 2.5em; margin: 30px 0; min-height: 80px; }
     input {
       text-align: center; 
       background-color: lightgreen; 
@@ -41,14 +41,14 @@
     <div class="row justify-content-center">
       <div class="col-12 col-md-8 game-container">
         
-        <h3 class="fw-bold text-success">Solve the Equations</h3>
+        <h3 class="fw-bold text-primary">Linear Equations Gauntlet</h3>
         <p class="text-muted fs-5" id="level-indicator">Level 1 of 4</p>
 
         <div id="equation-display"></div>
 
         <div id="play-area" class="row justify-content-center align-items-center mt-4">
-          <div class="col-auto text-end"><label class="fs-3 fw-bold"><i style="font-family: serif;">x</i> =</label></div>
-          <div class="col-auto"><input type="number" id="user-answer" class="form-control" step="any"></div>
+         <div class="col-auto text-end"><label class="fs-3 fw-bold"><i style="font-family: serif;">x</i> =</label></div>
+          <div class="col-auto"><input type="number" id="user-answer" class="form-control"></div>
           <div class="col-auto"><button id="check-btn" class="btn btn-primary btn-lg">Check</button></div>
         </div>
         
@@ -63,92 +63,83 @@
     var currentLevel = 0;
     var currentSolution = null;
 
-    // --- HELPER FUNCTIONS ---
-    function randomInteger(min, max) { 
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    function gcd(a, b) {
-        if (b) {
-            return gcd(b, a % b);
-        } else {
-            return Math.abs(a);
-        }
-    }
-
     // --- MATH GENERATORS ---
+    function gcd(x, y) {
+        x = Math.abs(x); y = Math.abs(y);
+        while (y) { let t = y; y = x % y; x = t; }
+        return x;
+    }
 
-    // Level 1: Pythagorean 
-    function makeQuestion1() {
-        var a = 0;
-        while (a % 2 == 0) {
-            a = randomInteger(7,20);
+    function gcd3(a, b, c) { return gcd(a, gcd(b, c)); }
+
+    function getRandomCoefficient() {
+        let val = 0;
+        while (val >= -1 && val <= 1) { 
+            val = Math.floor(Math.random() * 21) - 10; 
         }
-        var c = parseInt(a*a + 1)/2;
-        var b = Math.sqrt(c*c - a*a);
-
-        // Format: \sqrt{a^2 + b^2} = 
-        var expr = '\\( \\sqrt{' + a + '^2 + ' + b + '^2} \\)';
-        return { equation: expr, solution: c };
+        return val;
     }
 
-    // Level 2: Rational Equation
-    function makeQuestion2() {
-        var b = randomInteger(7,30);
-        var m = 1;
-        var a = b + m;
-
-        var diff = parseInt(a-b);
-        var product = a*b;
-        var x = product / diff;
-
-        // Format: 1/a + 1/x = 1/b
-        var expr = '\\( \\frac{1}{' + a + '} + \\frac{1}{x} = \\frac{1}{' + b + '} \\)';
-        return { equation: expr, solution: x };
-    }
-
-    // Level 3: Ratio and Sum
-    function makeQuestion3() {
-        var a = 8;
-        var b = 8;
-
-        // get a/b as a reduced fraction
-        while (a == b) {
-            a = randomInteger(10,25);
-            b = randomInteger(5,20);
-            var g = gcd(a,b);
-            a = a / g;
-            b = b / g;
+    // Level 1: ax + b = c
+    function generateLevel1() {
+        let a, b, c, x; let valid = false;
+        while (!valid) {
+            a = getRandomCoefficient(); b = getRandomCoefficient(); c = getRandomCoefficient();
+            if ((c - b) % a === 0 && c !== b && gcd3(a, b, c) === 1) {
+                x = (c - b) / a; valid = true;
+            }
         }
-          
-        var sum = parseInt(a + b);
-        var m = randomInteger(7,20);
-        var z = m * sum; // z is a multiple of a + b
-        var x = m * a; 
-     
-        // Format: x/y = a/b and x + y = z
-        var expr = '\\( \\frac{x}{y} = \\frac{' + a + '}{' + b + '} \\) &nbsp;&nbsp; \\text{and} &nbsp;&nbsp; \\( x + y = ' + z + ' \\)';
-        return { equation: expr, solution: x };
+        let signB = b > 0 ? "+" : "-";
+        return { equation: `\\( ${a}x ${signB} ${Math.abs(b)} = ${c} \\)`, solution: x };
     }
 
-    // Level 4: Triangular Numbers
-    function makeQuestion4() {
-       var n = randomInteger(19,45);
-       var s = +(n/2)*(n+1);
-
-       // Format: (x/2)(x + 1) = s
-       var expr = '\\( \\frac{x}{2} (x + 1) = ' + s + ' \\) &nbsp;&nbsp; \\text{(where } x > 0 \\text{)}';
-       return { equation: expr, solution: n };
+    // Level 2: a(x + b) + c = d
+    function generateLevel2() {
+        let a, b, c, d, x; let valid = false;
+        while (!valid) {
+            a = getRandomCoefficient(); b = getRandomCoefficient(); c = getRandomCoefficient(); d = getRandomCoefficient();
+            if ((d - c) % a === 0) {
+                x = ((d - c) / a) - b;
+                if (x !== 0) valid = true;
+            }
+        }
+        let signB = b > 0 ? "+" : "-"; let signC = c > 0 ? "+" : "-";
+        return { equation: `\\( ${a}(x ${signB} ${Math.abs(b)}) ${signC} ${Math.abs(c)} = ${d} \\)`, solution: x };
     }
 
+    // Level 3: x/a + b = c
+    function generateLevel3() {
+        let a, b, c, x; let valid = false;
+        while (!valid) {
+            a = getRandomCoefficient(); b = getRandomCoefficient(); c = getRandomCoefficient();
+            if (c !== b) { x = a * (c - b); valid = true; }
+        }
+        let signB = b > 0 ? "+" : "-";
+        let termX = a < 0 ? `-\\frac{x}{${Math.abs(a)}}` : `\\frac{x}{${a}}`;
+        return { equation: `\\( ${termX} ${signB} ${Math.abs(b)} = ${c} \\)`, solution: x };
+    }
+
+    // Level 4: (x+a)/(x+c) = d
+    function generateLevel4() {
+        let a, c, d, x; let valid = false;
+        while (!valid) {
+            a = getRandomCoefficient(); c = getRandomCoefficient(); d = getRandomCoefficient();
+            if ((d * c - a) % (1 - d) === 0) {
+                x = (d * c - a) / (1 - d);
+                if (x !== 0 && (x + c) !== 0) valid = true;
+            }
+        }
+        let signA = a > 0 ? "+" : "-"; let signC = c > 0 ? "+" : "-";
+        return { equation: `\\( \\frac{x ${signA} ${Math.abs(a)}}{x ${signC} ${Math.abs(c)}} = ${d} \\)`, solution: x };
+    }
 
     // --- GAME LOGIC ---
     function loadNextLevel() {
         let qData;
-        if (currentLevel === 0) qData = makeQuestion1();
-        else if (currentLevel === 1) qData = makeQuestion2();
-        else if (currentLevel === 2) qData = makeQuestion3();
-        else if (currentLevel === 3) qData = makeQuestion4();
+        if (currentLevel === 0) qData = generateLevel1();
+        else if (currentLevel === 1) qData = generateLevel2();
+        else if (currentLevel === 2) qData = generateLevel3();
+        else if (currentLevel === 3) qData = generateLevel4();
 
         currentSolution = qData.solution;
         
@@ -168,8 +159,7 @@
         loadNextLevel();
 
         $('#check-btn').click(function() {
-            // Using parseFloat in case any answers require decimals
-            let guess = parseFloat($('#user-answer').val());
+            let guess = parseInt($('#user-answer').val());
             
             if (isNaN(guess)) {
                 $('#feedback').html('<span class="text-danger">Please enter a number.</span>');
@@ -185,7 +175,7 @@
                 } else {
                     // Won all 4 levels
                     $('#level-indicator').text("Gauntlet Complete!");
-                    $('#equation-display').html('<span class="text-success fw-bold">⭐ All Equations Solved! ⭐</span>');
+                    $('#equation-display').html('<span class="text-success fw-bold">⭐ All Levels Cleared! ⭐</span>');
                     $('#play-area').hide();
                     $('#feedback').text("");
                     
--- raceGemini/bodmas.php
+++ raceGemini/bodmas.php
@@ -1,20 +1,18 @@
 <?php 
- 
-$question = isset($_POST['question']) ? $_POST['question'] : 'Delta Challenge';
-
+ $question = isset($_POST['question']) ? $_POST['question'] : 'Delta Challenge';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
-  <head>
- 
- 
+<head>
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <title>Progressive BODMAS Substitution</title>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js"></script>
-    
+
   <script type="text/x-mathjax-config">
     MathJax.Hub.Config({
       extensions: ["tex2jax.js"],
@@ -22,382 +20,186 @@ $question = isset($_POST['question']) ? $_POST['question'] : 'Delta Challenge';
       tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
     });
   </script>   
-  
-   <script type="text/javascript">
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-  </script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js"></script>
+  <script src="javascript/utilities.js"></script>
 
-  <script type="text/javascript" src="../javaScript/mathJax/MathJax-2.7.7/MathJax.js"></script>
-
-<link rel="stylesheet" href="raceGeminiStyles.css">
-
-<script src="javascript/utilities.js"></script>
-   
-
- <script type="text/x-mathjax-config">
-    MathJax.Hub.Config({
-      extensions: ["tex2jax.js"],
-      jax: ["input/TeX","output/HTML-CSS"],
-      tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
-    });
-  </script>   
-  
-   <script type="text/javascript">
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-  </script>
-  
-  <script type="text/javascript" src="../MathJax-2.7.5/MathJax.js"></script>
-
-<script src="javascript/utilities.js"></script>
-
-<title>6 Questions Junior</title>
-
-<style>
-
-
-
-
-
-
-
-h1 {
-font-weight: bolder; 
-   font-size: 24pt; 
-   
-   color:green;
-}
-
-h2 {
-font-weight: bolder; 
-   font-size: 20pt; 
-   
-   color:blue;
-
-}
-
-h3 {
-font-weight: bolder; 
-   font-size: 16pt; 
-   
-   color:blue;
-}
-
-h4 {
-font-weight: bold; 
-   font-size: 14pt; 
-   
-   color:orange;
-}
-
-
-
-p {
-font-weight: bold;
-font-style: italic;
-font-size: medium;
-}
-
-
-#message {font-size: 10pt ; font-style: italic;color: black ; text-align: justify;}
-
-#answer {
-            text-align: center;
-            background-color: lightblue;
-            font-size: 1.2em;
-            font-weight: bolder;
-}
-
-
-h4 {
-            text-align: center;
-            
-            font-size: 1.2em;
-            font-weight: bold;
-            color: black;
-}
-
-input {
-    display: inline-block; 
-    background-color: lightyellow; 
-    text-align: center; 
-    font-size: 1.2em; 
-    font-weight: bolder;
-    margin: 10px;
-    width: 4em;
-    height: 3em;
-
-
-}
-
-[id^=equation] {
-    font-weight: bolder;
-    color: black;
-    font-size: 1.2em;
-}
-
-p.parameter  {
-    display: inline-block;
-    color: blue;
-    font-size: 1em;
-    font-weight: bolder;
-
-}
-
-
-</style>
-
-
+  <style>
+    .game-container { text-align: center; margin-top: 20px; }
+    .variables-box { 
+        background-color: #e9ecef; 
+        border: 2px solid #0d6efd; 
+        border-radius: 8px; 
+        padding: 15px; 
+        margin-bottom: 20px;
+    }
+    #equation-display { font-size: 2.2em; margin: 30px 0; min-height: 80px; }
+    input {
+      text-align: center; 
+      background-color: lightgreen; 
+      font-size: 1.5em; 
+      font-weight: bold;
+      width: 5em;
+    }
+  </style>
 </head>
+
 <body>
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-8 game-container">
+        
+        <h3 class="fw-bold text-success">Substitution Gauntlet</h3>
+        
+        <div class="variables-box shadow-sm">
+            <h5 class="text-muted mb-3">Use these values to solve the equations:</h5>
+            <div class="d-flex justify-content-evenly fs-3 fw-bold text-primary">
+                <div><i style="font-family: serif; color: black;">a</i> = <span id="val-a"></span></div>
+                <div><i style="font-family: serif; color: black;">b</i> = <span id="val-b"></span></div>
+                <div><i style="font-family: serif; color: black;">c</i> = <span id="val-c"></span></div>
+                <div><i style="font-family: serif; color: black;">d</i> = <span id="val-d"></span></div>
+            </div>
+        </div>
 
-    <div class  = "container-fluid">
+        <p class="text-muted fs-5" id="level-indicator">Level 1 of 4</p>
 
-        <div class = "row">
-            <div class = "col-sm-12 c">
-                <p id = "stars"></p>
-                </div></div>
+        <div id="equation-display"></div>
 
-    <div class = "row">
-      <div class = "col-sm-12 c">
+        <div id="play-area" class="row justify-content-center align-items-center mt-4">
+          <div class="col-auto text-end"><label class="fs-3 fw-bold">Answer =</label></div>
+          <div class="col-auto"><input type="number" id="user-answer" class="form-control" step="any"></div>
+          <div class="col-auto"><button id="check-btn" class="btn btn-primary btn-lg">Check</button></div>
+        </div>
+        
+        <div id="feedback" class="mt-4 fs-5 fw-bold"></div>
 
-    <h1>Calculate</h1>
+      </div>
+    </div>
+  </div>
 
-    <h3>
-        a = <p class = "parameter" id = "a"></p>,
-        b = <p class = "parameter" id = "b"></p>,
-        c = <p class = "parameter" id = "c"></p>,
-        d = <p class = "parameter" id = "d"></p>
-    </h3>
+  <script type="text/javascript">
+    var questionID = '<?php echo $question; ?>';
+    var currentLevel = 0;
+    var currentSolution = null;
 
-    <h4>There are 3 marks for each question.</h4>
+    // Global variables for the math
+    var a, b, c, d;
 
-    
-  
-</div></div>
+    // --- HELPER FUNCTIONS ---
+    function randomInteger(min, max) { 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
+    // --- MATH GENERATORS ---
 
- <div class = "row">
-      <div class = "col- c">
-    <div id = "ex1">
-<label id = "equation1"></label>
-<input id = "solution1">
-<button id = "check1">Check 1</button>
-</div>
-</div></div>
+    // Level 1: (a + b) - (c + d)
+    function makeQuestion1() {
+        let x = (a + b) - (c + d);
+        let expr = '\\( (a + b) - (c + d) = \\)';
+        return { equation: expr, solution: x };
+    }
 
+    // Level 2: (a - b) - (c - d)
+    function makeQuestion2() {
+        let x = (a - b) - (c - d);
+        let expr = '\\( (a - b) - (c - d) = \\)';
+        return { equation: expr, solution: x };
+    }
 
+    // Level 3: (a^2 - d^2) - (b^2 - c^2)
+    function makeQuestion3() {
+        let x = (a*a - d*d) - (b*b - c*c);
+        let expr = '\\( (a^2 - d^2) - (b^2 - c^2) = \\)';
+        return { equation: expr, solution: x };
+    }
 
- <div class = "row">
-      <div class = "col- c">
-    <div id = "ex2">
-<label id = "equation2"></label>
-<input id = "solution2">
-<button id = "check2">Check 2</button>
-</div>
-</div></div>
+    // Level 4: (a - d)^3 + (b - c)^3
+    function makeQuestion4() {
+        let x = Math.pow((a - d), 3) + Math.pow((b - c), 3);
+        let expr = '\\( (a - d)^3 + (b - c)^3 = \\)';
+        return { equation: expr, solution: x };
+    }
 
+    // --- GAME LOGIC ---
+    function loadNextLevel() {
+        let qData;
+        if (currentLevel === 0) qData = makeQuestion1();
+        else if (currentLevel === 1) qData = makeQuestion2();
+        else if (currentLevel === 2) qData = makeQuestion3();
+        else if (currentLevel === 3) qData = makeQuestion4();
 
+        currentSolution = qData.solution;
+        
+        $('#level-indicator').text(`Level ${currentLevel + 1} of 4`);
+        $('#equation-display').html(qData.equation);
+        $('#user-answer').val('').focus();
+        $('#feedback').text('');
 
- <div class = "row">
-      <div class = "col- c">
-    <div id = "ex3">
-<label id = "equation3"></label>
-<input id = "solution3">
-<button id = "check3">Check 3</button>
-</div>
-</div></div>
+        // Instruct MathJax to re-render the new equation beautifully
+        if (typeof MathJax !== 'undefined') {
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, "equation-display"]);
+        }
+    }
 
+    $(document).ready(function() {
+        // 1. Generate the constants ONCE at the start of the game
+        a = randomInteger(5, 20);
+        b = randomInteger(5, 10);
+        c = randomInteger(5, 20);
+        d = randomInteger(5, 20);
 
- <div class = "row">
-      <div class = "col- c">
-    <div id = "ex4">
-<label id = "equation4"></label>
-<input id = "solution4">
-<button id = "check4">Check 4</button>
-</div>
-</div></div>
+        // 2. Display the constants in the banner
+        $('#val-a').text(a);
+        $('#val-b').text(b);
+        $('#val-c').text(c);
+        $('#val-d').text(d);
 
+        // 3. Kick off the first question
+        loadNextLevel();
 
+        // 4. Handle clicks
+        $('#check-btn').click(function() {
+            // Some answers can be negative, so we parse as float
+            let guess = parseFloat($('#user-answer').val());
+            
+            if (isNaN(guess)) {
+                $('#feedback').html('<span class="text-danger">Please enter a number.</span>');
+                return;
+            }
 
+            if (guess === currentSolution) {
+                currentLevel++; // Advance level
+                
+                if (currentLevel < 4) {
+                    $('#feedback').html('<span class="text-success">Correct! Loading next level...</span>');
+                    setTimeout(loadNextLevel, 800); 
+                } else {
+                    // Won all 4 levels
+                    $('#level-indicator').text("Gauntlet Complete!");
+                    $('#equation-display').html('<span class="text-success fw-bold">⭐ All Equations Solved! ⭐</span>');
+                    $('#play-area').hide();
+                    $('#feedback').text("");
+                    
+                    if (typeof handleCorrectAnswer === "function") {
+                        handleCorrectAnswer();
+                    } else if (typeof processWin === "function") {
+                        processWin(questionID);
+                    } else {
+                        alert("Great job! You answered all questions correctly.");
+                    }
+                }
+            } else {
+                $('#feedback').html('<span class="text-danger">Not quite. Try again!</span>');
+                $('#user-answer').val('').focus();
+            }
+        });
 
-
-</div>
-
+        // Allow pressing Enter to check answer
+        $('#user-answer').keypress(function(e) {
+            if(e.which == 13) { 
+                $('#check-btn').click(); 
+            }
+        });
+    });
+  </script>
 </body>
 </html>
-
-
-<script>
-// 1. GLOBAL VARIABLES (Must be defined cleanly)
-var answer = [];
-var correct = 0;
-var points = 0;
-// Global variables for canvas
-
-</script>
-
-<script type="text/javascript">
-    
-    function gcd(a, b) {
-   
-   a = Math.abs(a) ;
-   b = Math.abs(b) ;
-
-//alert('Now ' + a + ' ' + b);
-
-    if (a == 0)
-        return b;
-
-    if (b == 0)
-       return a ;  
-
-    while (b != 0) 
-    {
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
-    }
-
-    return a;
-}
-
-</script>
-
-<script type="text/javascript">
-    
-
-      function randomInteger(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-</script>
-
-
-
-<script type="text/javascript">
-    
-    function makeQuestion1(a,b,c,d)
-
-
-    {
-
-        x = (a+b) - (c+d);
-
-        var expr = '$ (a + b) - (c + d ) = $';
-
-        $('#equation1').html(expr);
-         MathJax.Hub.Queue(["Typeset", MathJax.Hub, "equation1" ]);
-
-console.log('q1',a,b,c,d,x);
-
-        return x;
-
-
-    }
-</script>
-
-<script type="text/javascript">
-    
-    function makeQuestion2(a,b,c,d)
-
-
-    {
-
-        x = (a - b) - (c - d);
-        var expr = '$ ( a - b) - (c - d) = $';
-        $('#equation2').html(expr);
-         MathJax.Hub.Queue(["Typeset", MathJax.Hub, "equation2" ]);
-
-console.log('q2',a,b,c,d,x);
-
-   return x;
-
-
-    }
-</script>
-
-
-<script type="text/javascript">
-    
-    function makeQuestion3(a,b,c,d)
-
-
-    {
-
-        x = (a*a - d*d) - (b*b - c*c);
-        var expr = '$ ( a^2  -  d^2 )  - (b^2 -  c^2 )  = $';
-        $('#equation3').html(expr);
-         MathJax.Hub.Queue(["Typeset", MathJax.Hub, "equation3" ]);
-
-console.log('q3',a,b,c,d,x);
-
-   return x;
-
-
-
-    }
-</script>
-
-
-<script>
-
-    function makeQuestion4(a,b,c,d)
-
-
-    {
-
-        x = (a-d)**3 + (b - c)**3;
-        var expr = '$ (a - d)^3  + (b - c)^3 = $';
-        $('#equation4').html(expr);
-         MathJax.Hub.Queue(["Typeset", MathJax.Hub, "equation4" ]);
-
-console.log('q4',a,b,c,d,x);
-
-   return x;
-
-    }
-</script>
-
-<script type="text/javascript">
-  
-    $(document).ready(function(){
-   
-    question = '<?php echo $question; ?>' ;
-// points = question.substr(-1);
-// calculate points on exit
-// $('#cancel').text("Exit");
-
-answer = [];
-
-a = randomInteger(5,20);
-b = randomInteger(5,10);
-c = randomInteger(5,20);
-d = randomInteger(5,20);
-
-
-answer[1] = makeQuestion1(a,b,c,d) ;
-answer[2] = makeQuestion2(a,b,c,d) ;
-answer[3] = makeQuestion3(a,b,c,d) ;
-answer[4] = makeQuestion4(a,b,c,d) ;
-
-correct = 0 ; // number correct;
-points = 0 ;
-
-// get values of a,b,c,d - global
-
-
-$('#a').text(a);
-$('#b').text(b);
-$('#c').text(c);
-$('#d').text(d);
-
-console.log(a,b,c,d);
-
-checkAnswer(4);
-  })
-
-
-</script>
-
-
-

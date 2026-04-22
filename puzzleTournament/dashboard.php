1bab456bd20716fa2f95b6f4d5db1333f734694c
John Thompson <john@teacherjohn.org>
2026-04-22 12:30:26 +0700

    upgrade pentomioes 4  random selections

--- puzzleTournament/dashboard.php
+++ puzzleTournament/dashboard.php
@@ -205,7 +205,7 @@ function getPuzzleState($num, $solvedArray) {
         style="bottom: 60%; left: 50%; background-image: url('images/wat_ounalom.jpg');" title="Turn the lights on">6</a>
 
         <?php $state7 = getPuzzleState(7, $solved); ?>
-        <a href="pentominoes.php" class="map-pin <?php echo $state7; ?>" 
+        <a href="pentominoes4.php" class="map-pin <?php echo $state7; ?>" 
         style="bottom: 70%; left: 40%; background-image: url('images/independenceMonument.jpg');" title="Solve it">7</a>
 
         <?php $state8 = getPuzzleState(8, $solved); ?>

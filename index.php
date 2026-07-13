ea2c347712c7d447a9193da599b6c1a78f2b0cda
John Thompson <john@teacherjohn.org>
2026-07-14 06:56:36 +0700

    added face scanner

--- index.php
+++ index.php
@@ -76,8 +76,8 @@
 
 <a href = "maps/mapsIndex.html" target = "_blank">
         <button class = "btn btn-warning" title = "TSP map">Map</button> </a>
-<a href = "maps/map.html" target = "_blank">
-        <button class = "btn btn-warning" title = "TSP map">Map KH</button> </a>
+<a href = "https://teacherjohn.org/teachableMachine/faceScanner.html" target = "_blank">
+        <button class = "btn btn-warning" title = "TSP map">Face scanner</button> </a>
 
  <div class="col-12 text-center">
             <a href="indexTechnology.php"><button class="btn btn-success">Web apps</button></a>
